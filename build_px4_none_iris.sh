027aacbee88a2e848262d5f6406237ddd823a6fb
Saar Buskila <saarbos@gmail.com>
2022-02-21 03:03:33 +0200

    Update content

--- build_px4_none_iris.sh
+++ build_px4_none_iris.sh
@@ -2,20 +2,12 @@
 
 # Build the px4 moudles and binaries
 make px4_sitl none_iris &> build.log &
-<<<<<<< HEAD
-# Get the pid of make
-PID=$!
-
-msg="Waiting for simulator to accept connection on TCP port 4560"
-# Waiting until port 4560 will show up in the log file
-=======
 
 # Get the pid of build procsess
 PID=$!
 
 msg="Waiting for simulator to accept connection on TCP port 4560"
 # Waiting until message will show up in the log file
->>>>>>> ee056f8 (Update content)
 until grep "$msg" build.log
 do
     echo "Waiting for the build process to be done..."
